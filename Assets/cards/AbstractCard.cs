1c01c18df5f21f052f7d596039ec9f0b9d36b214
ZhuohaoZeng <Zhuohao_Zeng@unc.edu>
2025-02-27 22:39:14 -0500

    Implement Card Prefab

--- Assets/cards/AbstractCard.cs
+++ Assets/cards/AbstractCard.cs
@@ -3,19 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class AbstractCard : ScriptableObject
     //STS implements Comparable<AbstractCard>
     //I wonder why did he need CompareTo Method?
     //Is there any spcial cases he need to compare if 2 card are the same?
 {
-    public CardType type;
+    public Sprite Sprite;
+    public List<CardType> type;
+    public string cardName;
+    public int damage;
+    public List<DamageType> damageType;
     public int cost;
     public int costForTurn;
     public int price;
     public int chargeCost;
     public Boolean isCostModified;
     public Boolean isCostModifiedForTurn;
-    public int damege;
     public int heal;
     public int draw;
 }
@@ -31,4 +35,9 @@ public enum CardType
     POWER,
     STATUS,
     CURSE
+}
+
+public enum DamageType
+{
+    NORMAL, THORNS, HP_LOSS
 }
